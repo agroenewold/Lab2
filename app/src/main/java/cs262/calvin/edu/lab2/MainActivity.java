3fd64b525aa05e47e1872483136df8e233eb5072
agroenewold <groenewold.andrew@gmail.com>
2015-10-01 22:56:36 -0400

    Lab 3 done

--- app/src/main/java/cs262/calvin/edu/lab2/MainActivity.java
+++ app/src/main/java/cs262/calvin/edu/lab2/MainActivity.java
@@ -1,8 +1,12 @@
 package cs262.calvin.edu.lab2;
 
 import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 public class MainActivity extends Activity {
@@ -16,8 +20,12 @@ public class MainActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        //getMenuInflater().inflate(R.menu.menu_main, menu);
+        //return true;
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -25,13 +33,33 @@ public class MainActivity extends Activity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+       // int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+       // if (id == R.id.action_settings) {
+         //   return true;
+       // }
 
-        return super.onOptionsItemSelected(item);
+       // return super.onOptionsItemSelected(item);
+            switch (item.getItemId()) {
+                case R.id.action_search:
+                    openSearch();
+                    return true;
+                case R.id.action_settings:
+                    openSettings();
+                    return true;
+                default:
+                    return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+    // Had to add these definitions
+    private void openSearch(){
+        startActivity(new Intent(SearchManager.INTENT_ACTION_GLOBAL_SEARCH));
+    }
+
+    private void openSettings(){
+        startActivity(new Intent(Settings.ACTION_SETTINGS));
     }
 }
